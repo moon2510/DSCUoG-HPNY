9c73bcb715215533eed614c925f261b2e9b2eda1
moon2510 <62028865+moon2510@users.noreply.github.com>
2021-02-10 14:38:46 +0700

    v2.3

--- index.php
+++ index.php
@@ -46,6 +46,61 @@
       </script>
       <div class="fb-like" data-share="true" data-width="450" data-show-faces="true">
       </div>
+      <script>
+         function statusChangeCallback(response) { // Called with the results from FB.getLoginStatus().
+            console.log('statusChangeCallback');
+            console.log(response); // The current login status of the person.
+            if (response.status === 'connected') { // Logged into your webpage and Facebook.
+               testAPI();
+            } else { // Not logged into your webpage or we are unable to tell.
+               document.getElementById('status').innerHTML = 'Please log ' +
+                  'into this webpage.';
+            }
+         }
+
+
+         function checkLoginState() { // Called when a person is finished with the Login Button.
+            FB.getLoginStatus(function(response) { // See the onlogin handler
+               statusChangeCallback(response);
+            });
+         }
+
+
+         window.fbAsyncInit = function() {
+            FB.init({
+               appId: '{app-id}',
+               cookie: true, // Enable cookies to allow the server to access the session.
+               xfbml: true, // Parse social plugins on this webpage.
+               version: '{api-version}' // Use this Graph API version for this call.
+            });
+
+
+            FB.getLoginStatus(function(response) { // Called after the JS SDK has been initialized.
+               statusChangeCallback(response); // Returns the login status.
+            });
+         };
+
+         function testAPI() { // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
+            console.log('Welcome!  Fetching your information.... ');
+            FB.api('/me', function(response) {
+               console.log('Successful login for: ' + response.name);
+               document.getElementById('status').innerHTML =
+                  'Thanks for logging in, ' + response.name + '!';
+            });
+         }
+      </script>
+
+
+      <!-- The JS SDK Login Button -->
+
+      <fb:login-button scope="public_profile,email" onlogin="checkLoginState();">
+      </fb:login-button>
+
+      <div id="status">
+      </div>
+
+      <!-- Load the JS SDK asynchronously -->
+      <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
       <div id="new-year"><svg id="new-year-svg" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 500 200">
 
             <defs>
